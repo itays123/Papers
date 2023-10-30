e45dc1ff9e03414fbaa344c321bbd8a66e1171ef
itays123 <itays2005@gmail.com>
2023-10-30 10:11:15 +0200

    20554-13-Q1-MultipleChoiceQuestion

---
+++ 20554 Advanced-Java/Assignment13/Q1/MultipleChoiceQuestion.java
@@ -0,0 +1,72 @@
+import java.util.Random;
+
+/**
+ * Represents a multiple choice question - a question with one correct option
+ * and {@code NUM_OF_INCORRECT_OPTIONS} incorrect ones.
+ */
+public class MultipleChoiceQuestion {
+
+    public static final int NUM_OF_INCORRECT_OPTIONS = 3;
+
+    private String question;
+    private String[] options;
+    private int correctOptionIndex;
+
+    /**
+     * Constructs a new MultipleChoiceQuestionObject
+     */
+    public MultipleChoiceQuestion(String question, String[] options, int correctOptionIndex) {
+        this.question = question;
+
+        if (options.length != NUM_OF_INCORRECT_OPTIONS + 1)
+            throw new IllegalArgumentException(
+                    "Number of options must be exactly " + (NUM_OF_INCORRECT_OPTIONS + 1));
+        if (correctOptionIndex < 0 || correctOptionIndex > NUM_OF_INCORRECT_OPTIONS)
+            throw new IllegalArgumentException(
+                    "Correct option index must be between 0 and " + NUM_OF_INCORRECT_OPTIONS);
+
+        this.options = options;
+        this.correctOptionIndex = correctOptionIndex;
+    }
+
+    /**
+     * Shuffle the order of the questions
+     */
+    public void shuffleOptionsOrder() {
+        Random rand = new Random();
+        for (int i = 0; i < options.length; i++) {
+            int randomIndexToSwap = rand.nextInt(options.length);
+            String temp = options[randomIndexToSwap];
+            options[randomIndexToSwap] = options[i];
+            options[i] = temp;
+
+            // if we swapped the correct answer, change correctAnswerIndex accordingly
+            if (correctOptionIndex == i)
+                correctOptionIndex = randomIndexToSwap;
+            else if (correctOptionIndex == randomIndexToSwap)
+                correctOptionIndex = i;
+        }
+    }
+
+    /**
+     * A getter for the question field
+     */
+    public String getQuestion() {
+        return question;
+    }
+
+    /**
+     * Get the options. Note that these might be shuffled
+     */
+    public String[] getOptions() {
+        return options;
+    }
+
+    /**
+     * Checks if the option index given matches the correct option index
+     */
+    public boolean checkOption(int optionIndex) {
+        return correctOptionIndex == optionIndex;
+    }
+
+}
