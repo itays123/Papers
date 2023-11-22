61621290a46b1173d8e2e9b63ab3bb9e7094382a
itays123 <itays2005@gmail.com>
2023-11-22 16:33:13 +0200

    20554-14-q2-finish

--- 20554 Advanced-Java/Assignment14/Q2/ContactListCell.java
+++ 20554 Advanced-Java/Assignment14/Q2/ContactListCell.java
@@ -1,5 +1,13 @@
+import java.io.IOException;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 
 /**
@@ -7,13 +15,52 @@ import javafx.scene.layout.HBox;
  */
 public class ContactListCell extends ListCell<Contact> {
 
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private TextField phoneNumberField;
+
+    @FXML
+    private Button removeBtn;
+
+    @FXML
+    private HBox pane;
+
+    public ContactListCell() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ContactListCell.fxml"));
+        loader.setController(this);
+
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void updateItem(Contact item, boolean empty) {
         super.updateItem(item, empty);
-        if (item == null)
-            setGraphic(new HBox());
-        else
-            setGraphic(new HBox(new Label(item.getName())));
+        if (empty || item == null) {
+            setGraphic(null);
+            return;
+        }
+
+        nameLabel.setText(item.getName());
+        phoneNumberField.setText(item.getPhoneNumber());
+
+        setGraphic(pane);
     }
 
+    public void setOnPhoneNumberUpdate(BiConsumer<String, String> updateHandler) {
+        phoneNumberField.textProperty()
+                .addListener((prop, oldValue, newValue) -> updateHandler.accept(nameLabel.getText(), newValue));
+    }
+
+    public void setOnDelete(Consumer<String> deleteHandler) {
+        removeBtn.setOnAction(event -> {
+            deleteHandler.accept(nameLabel.getText());
+        });
+
+    }
 }
--- 20554 Advanced-Java/Assignment14/Q2/PhoneBookController.java
+++ 20554 Advanced-Java/Assignment14/Q2/PhoneBookController.java
@@ -3,14 +3,20 @@ import java.util.stream.Collectors;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
 
 public class PhoneBookController {
 
     private PhoneBook phoneBook;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField phoneNumberField;
 
     @FXML
     private TextField searchArea;
@@ -28,7 +34,15 @@ public class PhoneBookController {
         contacts = FXCollections.observableArrayList();
         contactList.setItems(contacts);
         contactList.setCellFactory(contact -> {
-            return new ContactListCell();
+            ContactListCell cell = new ContactListCell();
+            cell.setOnDelete((name) -> {
+                phoneBook.remove(name);
+                rerenderContacts();
+            });
+            cell.setOnPhoneNumberUpdate((name, phoneNumber) -> {
+                phoneBook.put(name, phoneNumber);
+            });
+            return cell;
         });
 
         // subscribe to text area changes
@@ -46,6 +60,11 @@ public class PhoneBookController {
         contacts.addAll(phoneBook.search(query).collect(Collectors.toList()));
     }
 
+    void rerenderContacts() {
+        contacts.clear();
+        renderContacts(searchArea.getText());
+    }
+
     /**
      * Try to load phone book
      */
@@ -55,6 +74,19 @@ public class PhoneBookController {
             System.out.println(phoneBook.entries);
         } catch (IOException e) {
             phoneBook = new PhoneBook();
+        }
+    }
+
+    @FXML
+    void insertButtonClicked(ActionEvent event) {
+        String name = nameField.getText();
+        String phoneNumber = phoneNumberField.getText();
+        if (!name.trim().isEmpty() && !phoneNumber.trim().isEmpty()) {
+            phoneBook.put(name, phoneNumber);
+            nameField.setText("");
+            phoneNumberField.setText("");
+            searchArea.setText(""); // show a list of all contacts. Should trigger re-render
+            rerenderContacts(); // will display in alphabetical order
         }
     }
 
