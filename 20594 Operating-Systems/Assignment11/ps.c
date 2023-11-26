023826b2aa01094a6bdd4fcfeb45544635ce15ce
itays123 <itays2005@gmail.com>
2023-11-26 20:27:23 +0200

    20594-11-q1

---
+++ 20594 Operating-Systems/Assignment11/ps.c
@@ -0,0 +1,17 @@
+/**
+ * Main ps file made by Itay Schechner 328197462
+ */
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(int argc, char *argv[])
+{
+
+    if (cps162() != 0)
+    {
+        printf(2, "ps: Failed to execute command");
+    }
+
+    exit();
+}
